442f86b7758f48213141ac8d2934488acb45f8c5
Raquib <r.raquib01@gmail.com>
2023-08-12 12:06:23 -0600

    navbar rafactors

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Links() {
     return <>
-        <div className="flex justify-center mt-1">
+        <div className="flex justify-center sm:-mt-4 tracking-wide">
           <Link href="https://github.com/raquibk" className="underline mx-4" target="_blank" rel="noopener noreferrer"> Github </Link>
           <Link href="/Lavani_Resume.pdf" className="underline mx-4" target="_blank" rel="noopener noreferrer"> Resume </Link>
           <Link href="https://github.com/raquibk" className="underline mx-4" target="_blank" rel="noopener noreferrer"> LinkedIn </Link>

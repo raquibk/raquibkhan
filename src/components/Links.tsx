29b149eaabb5efdb8ef9f63187265dd5b0f00d05
Raquib <r.raquib01@gmail.com>
2024-05-23 16:40:07 -0600

    fix: corrected linkedin link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,7 +5,7 @@ export function Links() {
         <div className="flex justify-center sm:-mt-4 tracking-wide">
           <Link href="https://github.com/raquibk" className="underline mx-4" target="_blank" rel="noopener noreferrer"> Github </Link>
           <Link href="/Lavani_Resume.pdf" className="underline mx-4" target="_blank" rel="noopener noreferrer"> Resume </Link>
-          <Link href="https://github.com/raquibk" className="underline mx-4" target="_blank" rel="noopener noreferrer"> LinkedIn </Link>
+          <Link href="https://www.linkedin.com/in/lavani/" className="underline mx-4" target="_blank" rel="noopener noreferrer"> LinkedIn </Link>
         </div>
     </>
 }
